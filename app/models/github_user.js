2dfc7387780f9549645a1c1786d5e253d6478f77
Alex <amurphy345@gmail.com>
2016-06-26 15:30:55 -0600

    findOneAndUpdate working for user collection

--- app/models/github_user.js
+++ app/models/github_user.js
@@ -1,5 +1,7 @@
 // model for github users
 var mongoose = require('mongoose')
+var findOrCreate = require('mongoose-findorcreate')
+
 var Schema = mongoose.Schema
 var githubUserSchema = new Schema({
   profileInformation : Object,
@@ -7,5 +9,6 @@ var githubUserSchema = new Schema({
   userAvailableRepos: Array
 })
 // include find or create plugin
+githubUserSchema.plugin(findOrCreate)
 
 module.exports = mongoose.model('githubUser', githubUserSchema)
--- app/routes.js
+++ app/routes.js
@@ -59,26 +59,64 @@ router.route('/githubUser')
           json: true
         }, function(err, response, profile) {
           // user profile is returned from github. check for user in db, if no user create one.
-          var githubUser = new githubUserModel()
-          githubUser.profileInformation = profile
           // next, fetch available repos from github and insert
-
-
           github.authenticate({
             type: "oauth",
             token: accessToken.access_token
           })
-          github.repos.getAll({}, function(err, response) {
+          github.repos.getAll({}, function(err, githubRepoResponse) {
             // TODO filter out user selected repos before appending to databaseConfig
-            githubUser.userAvailableRepos = response
+            var newUser = {}
+            newUser.profileInformation = profile
+            newUser.userAvailableRepos = githubRepoResponse
+            newUser.profileInformation.bearer_token = accessToken.access_token
+            githubUserModel.findOneAndUpdate({
+              'profileInformation.id' : profile.id
+            }, newUser, {upsert: true},
+            (error, document) => {
+              console.log(document);
+            })
+
+            // (err, githubUserFromDb) => {
+            //   // does our user collection already exist?
+            //   if(githubUserFromDb.length > 0){
+            //     // user collection exists. update token
+            //     console.log(githubUserFromDb[0].profileInformation);
+            //     githubUserFromDb[0].profileInformation.test = 'accessToken.access_token'
+            //     githubUserFromDb[0].save((e) => {console.log(e)})
+            //   } else {
+            //     // user colleciton does not exist. create user with available repos and profile data
+            //     var githubUser = new githubUserModel()
+            //     githubUser.profileInformation = profile
+            //     githubUser.profileInformation.bearer_token = accessToken.access_token
+            //     githubUser.userAvailableRepos = response
+            //     githubUser.save((e) => {console.log(e)})
+            //
+            //   }
+            //   console.log(err);
+            // })
+
+            //
+            // githubUserModel.findOrCreate({
+            //   profileInformation: profile,
+            //   userAvailableRepos: response
+            // })
+
             // save user to db.
-            githubUser.save((e) => {console.log(e)})
 
           })
           // return user collection
-          res.send(gitHubUser)
+          res.send('gitHubUser')
         })
       })
   })
+  // .get(function(req, res) {
+  //   githubUserModel.find(function(err, bears) {
+  //       if (err)
+  //           res.send(err);
+  //
+  //       res.json(bears);
+  //   });
+// });
 
 module.exports = router
