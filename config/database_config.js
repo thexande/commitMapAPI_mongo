422876884c43d298cf99b255bdb6a4b6a45f6a7b
Alex <amurphy345@gmail.com>
2016-06-27 10:34:35 -0600

    heroku config test 8 with pace

--- config/database_config.js
+++ config/database_config.js
@@ -1,4 +1,4 @@
 var mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/commitmap')
-// mongoose.connect('mongodb://heroku_n4zqsq4f:itl80p0kj1eo2n5oi1g9o8qk95@ds023674.mlab.com:23674/heroku_n4zqsq4f')
+// mongoose.connect('mongodb://localhost/commitmap')
+mongoose.connect('mongodb://heroku_n4zqsq4f:itl80p0kj1eo2n5oi1g9o8qk95@ds023674.mlab.com:23674/heroku_n4zqsq4f')
 module.exports = mongoose
